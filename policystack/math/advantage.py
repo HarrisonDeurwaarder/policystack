8c9a1501bc258b33ba3dc5324ae2a35e0bb9cafa
CentralLoaf <hdwater2@gmail.com>
2026-07-22 18:04:41 -0700

    tweaks math function signatures and contents to conform to DynamicTerm changes

--- policystack/math/advantage.py
+++ policystack/math/advantage.py
@@ -1,47 +1,58 @@
 import torch
 import torch.nn as nn
 
+from config import resolve
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from config import DynamicTerm
+
 
 def gae(
     rewards: torch.Tensor,
-    expected_values: torch.Tensor,
+    values: torch.Tensor,
+    next_values: torch.Tensor,
     dones: torch.Tensor,
-    discount_factor: float,
-    gae_decay: float,
-    **kwargs
+    discount_factor: float | DynamicTerm,
+    gae_decay: float | DynamicTerm,
 ) -> torch.Tensor:
     """
-    compute generalized advantage estimates
+    Computes generalized advantage estimates
 
     Args:
-        rewards (torch.Tensor): episode rewards of the policy (B, 1)
-        expected_values (torch.Tensor): critic's expected values of the policy at each state (B+1, 1)
-        dones (torch.Tensor): boolean flags indicating whether an episode is done (1) or not (0) (B, 1)
+        rewards (torch.Tensor): episode rewards of the policy (B, E)
+        values (torch.Tensor): critic's expected values of the policy at each state (B, E)
+        next_values (torch.Tensor): values at the next consecutive state (B, E)
+        dones (torch.Tensor): boolean flags indicating whether an episode is done (1) or not (0) (B, E)
         discount_factor (float): discount factor
         gae_decay (float): gae decay
 
     Returns:
-        torch.Tensor: advantage estimates
+        torch.Tensor: advantage estimates (B, E)
     """
     # td residual component (low variance)
     # takes the difference between the netted rewards (bootstrapped with the next state to determine value) and the critic's expected value
-    td_res = td_residual(rewards, expected_values, dones, discount_factor)
+    td_res = td_residual(rewards, values, next_values, dones, discount_factor)
     
     # recursive monte carlo component (low bias)
     # computes the discounted value of a finite episode directly
-    advantages = torch.zeros_like(expected_values) # an additional dimension is added to ease code complexity
+    adv_dims = rewards.shape()
+    adv_dims[-1] += 1 # an additional dimension is added to ease code complexity
+    advantages = torch.zeros(adv_dims)
     for t in range(rewards.size(-1) - 1, -1, -1): # iterate backwards from T to 0, inclusive
-        advantages[..., t] = td_res[..., t] + discount_factor * gae_decay * advantages[..., t+1]
+        advantages[..., t] = td_res[..., t] + resolve(discount_factor) * resolve(gae_decay) * advantages[..., t+1]
         
     return advantages[..., :-1]
 
 
 def td_residual(
     rewards: torch.Tensor,
-    expected_values: torch.Tensor,
+    values: torch.Tensor,
+    next_values: torch.Tensor,
     dones: torch.Tensor,
-    discount_factor: float,
-    **kwargs
+    discount_factor: float | DynamicTerm,
 ) -> torch.Tensor:
     """
     Computes the TD residual advantages
@@ -52,24 +63,24 @@ def td_residual(
     TD residual is, however, high-biased because of its reliance on the policy's own predictions to serve as the ground-truth
 
     Args:
-        rewards (torch.Tensor): episode rewards of the policy (B, 1)
-        expected_values (torch.Tensor): critic's expected values of the policy at each state (B+1, 1)
-        dones (torch.Tensor): boolean flags indicating whether an episode is done (1) or not (0) (B, 1)
+        rewards (torch.Tensor): episode rewards of the policy (B, E)
+        values (torch.Tensor): critic's expected values of the policy at each state (B, E)
+        next_values (torch.Tensor): values at the next consecutive state (B, E)
+        dones (torch.Tensor): boolean flags indicating whether an episode is done (1) or not (0) (B, E)
         discount_factor (float): discount factor
 
     Returns:
-        torch.Tensor: advantage estimates
+        torch.Tensor: advantage estimates (B, E)
     """
-    advantages = rewards + discount_factor * expected_values[..., 1:] (1 - dones) - expected_values[..., :-1]
+    advantages = rewards + resolve(discount_factor) * values * (1 - dones) - next_values[..., :-1]
     return advantages
 
 
 def monte_carlo(
     rewards: torch.Tensor,
-    expected_values: torch.Tensor,
+    values: torch.Tensor,
     dones: torch.Tensor,
-    discount_factor: float,
-    **kwargs
+    discount_factor: float | DynamicTerm,
 ) -> torch.Tensor:
     """
     Computes the monte carlo advantages
@@ -79,18 +90,18 @@ def monte_carlo(
     resulting in noisy advantage signals
 
     Args:
-        rewards (torch.Tensor): episode rewards of the policy (B, 1)
-        expected_values (torch.Tensor): critic's expected values of the policy at each state (B+1, 1)
-        dones (torch.Tensor): boolean flags indicating whether an episode is done (1) or not (0) (B, 1)
+        rewards (torch.Tensor): episode rewards of the policy (B, E)
+        values (torch.Tensor): critic's expected values of the policy at each state (B, E)
+        dones (torch.Tensor): boolean flags indicating whether an episode is done (1) or not (0) (B, E)
         discount_factor (float): discount factor
 
     Returns:
-        torch.Tensor: advantages
+        torch.Tensor: advantages (B, E)
     """
-    returns = torch.zeros_like(expected_values) # (B+1, 1)
+    returns = torch.zeros_like(values) # (B, E)
     # recursively compute ground-truth returns
     for t in range(rewards.size(-1) - 1, -1, -1):
-        returns[..., t] = rewards[..., t] + discount_factor * returns[..., t+1] * (1 - dones[..., t])
+        returns[..., t] = rewards[..., t] + resolve(discount_factor) * returns[..., t+1] * (1 - dones[..., t])
     # then compute the difference
-    advantages = returns[..., :-1] - expected_values
+    advantages = returns - values
     return advantages
--- policystack/math/objective.py
+++ policystack/math/objective.py
@@ -2,25 +2,32 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from config import resolve
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from config import DynamicTerm
+
 
 def clipped_surrogate_objective(
     log_prob: torch.Tensor,
     old_log_prob: torch.Tensor,
     advantage: torch.Tensor,
-    clipping_param: float = 0.2,
-    **kwargs
+    clipping_param: float = 0.2 | DynamicTerm
 ) -> torch.Tensor:
     """
-    compute the policy objective at a state for PPO
+    Computes the policy objective at a state for PPO
     
     Args:
-        log_prob (torch.Tensor): log probability of the chosen action being chosen in the current policy (B, 1)
-        old_log_prob (torch.Tensor): log probability of the chosen action being chosen in the old policy (B, 1)
-        advantage (torch.Tensor): benefit of the policy's choice over what was expected by the critic (B, 1)
+        log_prob (torch.Tensor): log probability of the chosen action being chosen in the current policy (B, E)
+        old_log_prob (torch.Tensor): log probability of the chosen action being chosen in the old policy (B, E)
+        advantage (torch.Tensor): benefit of the policy's choice over what was expected by the critic (B, E)
         clipping_param (float): amount of deviation from the old policy (expressed as a ratio of probabilities) that is accepted
         
     Returns:
-        torch.Tensor: the objective (B, 1)
+        torch.Tensor: the objective (B, E)
     """
     # ratio between current policy and frozen policy aids in preventing excessive updates
     # logarithmic properties are used to smooth computations for extremely small probabilities
@@ -33,7 +40,7 @@ def clipped_surrogate_objective(
     objective = torch.min(
         advantage * ratio,
         advantage * torch.clip(
-            ratio, 1 - clipping_param, 1 + clipping_param
+            ratio, 1 - resolve(clipping_param), 1 + resolve(clipping_param)
         )
     )
     return objective.mean() # reduce to scalar
@@ -44,35 +51,58 @@ def clipped_surrogate_with_entropy(
     old_log_prob: torch.Tensor,
     advantage: torch.Tensor,
     entropy: float,
-    clipping_param: float = 0.2,
-    entropy_coef: float = 0.01,
-    **kwargs
+    clipping_param: float | DynamicTerm,
+    entropy_coef: float | DynamicTerm,
 ) -> torch.Tensor:
     objective = clipped_surrogate_objective(
         log_prob=log_prob, old_log_prob=old_log_prob, advantage=advantage, clipping_param=clipping_param
     )
     # add the entropy term
-    return objective + torch.mean(entropy_coef * entropy)
+    return objective + torch.mean(resolve(entropy_coef) * entropy)
+    
     
 def critic_mse(
     expected_value: torch.Tensor,
     old_expected_value: torch.Tensor,
-    advantage: torch.Tensor,
-    **kwargs
+    advantage: torch.Tensor
 ) -> torch.Tensor:
     """
-    compute the value loss at a state for AC algorithms
+    Computes the value loss at a state for AC algorithms
 
     Args:
-        expected_value (torch.Tensor): critic expected value of the policy given the state (B, 1)
-        old_expected_value (torch.Tensor): old critic expected value of the policy given the state (B, 1)
-        advantage (torch.Tensor): benefit of the policy's choice over what was expected by the critic (B, 1)
+        expected_value (torch.Tensor): critic expected value of the policy given the state (B, E)
+        old_expected_value (torch.Tensor): old critic expected value of the policy given the state (B, E)
+        advantage (torch.Tensor): benefit of the policy's choice over what was expected by the critic (B, E)
 
     Returns:
-        torch.Tensor: the loss (B, 1)
+        torch.Tensor: the loss (B, E)
     """
     loss = F.mse_loss(
         expected_value,
         old_expected_value + advantage
     )
     return loss
+
+
+def msbe(
+    reward: torch.Tensor,
+    value: torch.Tensor,
+    next_value: torch.Tensor,
+    done: torch.Tensor,
+    discount_factor: float | DynamicTerm
+) -> torch.Tensor:
+    """
+    Computes the mean squared bellman error (td loss) of a value prediction
+    MSBE bootstraps a prediction using td error, and as such has low variance and high bias
+
+    Args:
+        reward (torch.Tensor): episode rewards of the policy (B, E)
+        value (torch.Tensor): value function's expected value of the state or state-action (B, E)
+        next_value (torch.Tensor): value function's expected value of the next state (B, E)
+        done (torch.Tensor): boolean flags indicating whether an episode is done (1) or not (0) (B, E)
+        discount_factor (float): discount factor
+
+    Returns:
+        torch.Tensor: the loss (B, E)
+    """
+    loss = F.mse_loss(value, reward + (1 - done) * resolve(discount_factor) * next_value)
