c9e85ca28c13a505e014448d5a68902f11d57b5f
CentralLoaf <hdwater2@gmail.com>
2026-07-22 17:58:22 -0700

    enables effective use of DynamicTerms outside of a config subclass and linked to the same TrainingState

--- policystack/config.py
+++ policystack/config.py
@@ -11,121 +11,46 @@ from __future__ import annotations
 from typing import Callable, Any, TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from training import TrainingContext
+    from training import TrainingState
+    
+    
+def resolve(v: DynamicTerm) -> Any:
+    # access as a regular variable, unless it is a DynamicTerm then access with updated value
+    # this should be used to access any variables that may have reason to follow a curriculum
+    return v.get() if isinstance(v, DynamicTerm) else v
     
 
 class DynamicTerm:
     """
     Modifies configuration variables according to training context
+    It is good practice to instantiate a DynamicTerm under a TrainingState object in order to avoid specifying the training state, and for clarity
     """
-    def __init__(self, callback: Callable) -> None:
+    def __init__(self, callback: Callable, state: TrainingState) -> None:
         self.callback = callback
+        self.state = state
         
-    def get(self, ctx: TrainingContext) -> Any:
+    def get(self) -> Any:
         # several potential arguments 
-        return self.callback(ctx)
+        return self.callback(self.state)
     
-    # the following are factory methods with common numerical annealing functions
+    # the following return common callback functions for numerical terms
     
-    @classmethod
-    def linear_decay(cls, start: float, end: float) -> DynamicTerm: # type: ignore
+    @staticmethod
+    def linear_decay(start: float, end: float) -> Callable:
         linear_fn = lambda ctx: start + (end - start) * ctx.progress
-        return cls(linear_fn)
+        return linear_fn
     
-    @classmethod
-    def expo_decay(cls, start: float, end: float) -> DynamicTerm: # type: ignore
+    @staticmethod
+    def expo_decay(start: float, end: float) -> Callable:
         expo_fn = lambda ctx: start * (start / end) ** ctx.progress
-        return cls(expo_fn)
+        return expo_fn
     
-    @classmethod
-    def step_decay(cls, start: float, end: float, step: float, latency: int) -> DynamicTerm: # type: ignore
+    @staticmethod
+    def step_decay(start: float, end: float, step: float, latency: int) -> Callable:
         step_fn = lambda ctx: max(start - (ctx.steps // latency) * step, end)
-        return cls(step_fn)
+        return step_fn
     
-    @classmethod
-    def constant(cls, value: Any) -> DynamicTerm: # type: ignore
+    @staticmethod
+    def constant(value: Any) -> Callable:
         const_fn = lambda ctx: value
-        return cls(const_fn)
-    
-
-
-@dataclass
-class Config:
-    # communication bridge; contains training context
-    ctx: TrainingContext
-    
-    def __getattribute__(self, name: str):
-        attr = getattr(self, name)
-        # get updated attribute value
-        return attr.get(self.ctx)
-
-
-
-@dataclass
-class PPOConfig(Config):
-    """
-    Config template for PPO
-    """
-    # network architecture parameters
-    # for the state-action function, the return value of forward() must
-    # be keyed {"continuous": ..., "discrete": ...}
-    # else, all actions will be assumed to be continous and may impose downstream errors
-    actor: nn.Module
-    critic: nn.Module
-    # assumes that actor/critic are trained seperately
-    # i.e. no shared backbone
-    actor_op: optim.Optimizer # note that learning rate scheduling is done within the optimizers; other curriculum
-    critic_op: optim.Optimizer
-    
-    # enviornment must follow gymnassium convention
-    # step(action) -> (obs, reward, term, trunc, info)
-    # reset(seed=None) -> (obs, info)
-    environment = field(default=MISSING)
-    
-    # number of times transitions from each rollout are iterated over
-    epochs: int = 10
-    # number of collect -> train cycles
-    iterations: int = 200
-    batch_size: int = 64
-    # number of steps collected in rollout phase
-    rollout_length: int = 1024
-    
-    # alloted ratio-difference between the target policy and trained policy
-    # prevents caatstrophic poicy collapse by limited the amount the policy can learn in on cycle
-    #clipping_param: float = 0.2
-    
-    policy_objective_fn: Callable = clipped_surrogate_with_entropy
-    advantage_fn: Callable = gae
-    critic_loss_fn: Callable = critic_mse
-    
-    policy_objective_params: dict[str, Any] = field(default={
-        "clipping_param": 0.2, "entropy_coef": 0.01
-    })
-    advantage_params: dict[str, Any] = field(default={
-        "discount_factor": 0.99, "gae_decay": 0.98
-    })
-    critic_loss_fn: dict[str, Any] = field(default={})
-    
-    # enables the use of a single optimizer on a weighted sum of the policy and value objectives; use with a shared backbone
-    # otherwise, two 
-    #couple_objectives: bool = True
-    # compresses the range of designated variance outputs to (0, inf)
-    #exponentiate_variance: bool = True
-    
-    
-
-@dataclass
-class DQNConfig:
-    net: nn.Module = field(default=MISSING)
-    # enables exploration in the dqn; policy with a probability of epsilon selects a random action
-    epsilon_fn: Callable | float = field(
-        default=lambda t: max(0.01, 1.0 - t / 100_000)
-    )
-    
-    # number of collection + refinement cycles
-    iterations: int = field(default=100_000)
-    # define the ratio between environment and gradient update steps
-    n_collections_per_iter: int = field(default=1)
-    n_refinements_per_iter: int = field(default=1)
-    
-    batch_size: int = field(default=64)
+        return const_fn
